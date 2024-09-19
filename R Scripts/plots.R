5d0e332d3bbc5503266d28db72f4afb473ea9b8a
bezerra-ag <bezerraag.acad@gmail.com>
2024-09-19 11:14:35 -0300

    new graph and default function

--- R Scripts/plots.R
+++ R Scripts/plots.R
@@ -3,7 +3,12 @@ setwd   ("../R Scripts")
 source  ("data.R")
 require ("ggplot2")
 
-ggplot(d[d$Function == "Transporte",], aes(x = Year, y = Value.Planned)) +
+ggplot(d, aes(x = Year, y = Value.Paid/Value.Planned)) +
   geom_boxplot(outliers = F) +
-  coord_cartesian(ylim = c(0, 75000000)) +
-  theme(axis.text.x = element_text(angle = 90))
+  facet_wrap(~ Function) +
+  theme(axis.text.x = element_text(angle = 90)) +
+  labs(
+    title = "Razão de Valores Pagos por Valores Empenhados das Emendas Parlamentares dos últimos 10 anos",
+    x = "Ano",
+    y = "Razão de Valor Pago por Valor Empenhado"
+  )
